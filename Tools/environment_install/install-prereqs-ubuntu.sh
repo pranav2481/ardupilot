2905257ece00b660727686a39b69fb5b8afed38e
Pranav <pranav2481@proton.me>
2025-01-20 14:33:43 +0530

    remove sudo checnk

--- Tools/environment_install/install-prereqs-ubuntu.sh
+++ Tools/environment_install/install-prereqs-ubuntu.sh
@@ -3,11 +3,6 @@ echo "---------- $0 start ----------"
 set -e
 set -x
 
-if [ $EUID == 0 ]; then
-    echo "Please do not run this script as root; don't sudo it!"
-    exit 1
-fi
-
 OPT="/opt"
 # Ardupilot Tools
 ARDUPILOT_TOOLS="Tools/autotest"
